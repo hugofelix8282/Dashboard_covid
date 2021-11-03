38182670c2f0df5061ad1cfc7841a9ffb092bac1
Hugo Felix <77499045+hugofelix8282@users.noreply.github.com>
2021-11-03 16:08:32 -0300

    Update covid.py

--- covid.py
+++ covid.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pylab as plty
 import seaborn as sns
-!pip install streamlit
 import pandas as pd
 import numpy as np
 import streamlit as st
